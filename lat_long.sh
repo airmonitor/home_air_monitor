f9c7a7a1bf22f61872b751fca02ebdef3c3eee3c
airmonitor <kontakt@skyphoto.pro>
2017-08-12 22:59:54 +0200

    Update lat_long.sh

--- lat_long.sh
+++ lat_long.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-dos2unix /bot/airmonitor.txt
+dos2unix /boot/airmonitor.txt
 lat=`awk 'NR==1{print $1}' /boot/airmonitor.txt`
 long=`awk 'NR==2{print $1}' /boot/airmonitor.txt`
 echo $lat
