13ab1f55beeb3ad5c90016e69187a035c043ebf5
airmonitor <kontakt@skyphoto.pro>
2017-08-12 22:11:14 +0200

    Create lat_long.sh

---
+++ lat_long.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+dos2unix /bot/airmonitor.txt
+lat=`awk 'NR==1{print $1}' /boot/airmonitor.txt`
+long=`awk 'NR==2{print $1}' /boot/airmonitor.txt`
+echo $lat
+echo $long
+sed  -i "/lat/s/000000/${lat}/g" /etc/configuration/configuration.data
+sed  -i "/long/s/000000/${long}/g" /etc/configuration/configuration.data
