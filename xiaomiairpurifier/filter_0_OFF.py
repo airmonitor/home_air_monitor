261d069334464cde368c7567d456f143f5e11ba0
airmonitor <kontakt@skyphoto.pro>
2017-11-30 11:27:13 +0100

    Add files via upload

---
+++ xiaomiairpurifier/filter_0_OFF.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.6
+#coding: utf-8
+
+import os
+from configparser import ConfigParser
+parser = ConfigParser()
+parser.read('/etc/configuration/configuration.data')
+filter_0_IP_address=(parser.get('airpurifier', 'filter_0_IP_address'))
+os.system('sudo screen -S airpurifieron -d -m bash /etc/configuration/xiaomiairpurifier/airpurifiercontrol.sh ' + filter_0_IP_address + ' off')
