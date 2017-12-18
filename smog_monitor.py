b85688dfbf62635821d062c548cbae3c082f0997
tomasz.szuster+github@gmail.com </bY|5(!7]Fa+Yk*XyPN)^%?ar@8;&Hz>
2017-12-18 10:23:52 +0100

    Update

--- smog_monitor.py
+++ smog_monitor.py
@@ -40,52 +40,44 @@ sensor.reset()
 sensor.workstate = SDS011.WorkStates.Measuring
 time.sleep(30)
 
-os.system('rm /mnt/ramdisk_ram0/ppm10_average_sensor0')
-os.system('rm /mnt/ramdisk_ram0/ppm25_average_sensor0')
 
-count = 0
-while (count < 30):
+COUNT = 0
+pm25_values = []
+pm10_values = []
+
+while (COUNT < 30):
 
     values = sensor.get_values()
     print("Values measured: PPM10:", values[0], "PPM2.5:", values[1])
-    count += 1
+    pm25_values.append(values[1])
+    pm10_values.append(values[0])
+    COUNT += 1
     time.sleep(1)
 
-    f_average_PM10 = open('/mnt/ramdisk_ram0/ppm10_average_sensor0', 'a')
-    ppm10 = str(values[0])
-    f_average_PM10.write(ppm10 + '\n')
-    f_average_PM10.close()
-
-    f_average_PM25 = open('/mnt/ramdisk_ram0/ppm25_average_sensor0', 'a')
-    ppm25 = str(values[1])
-    f_average_PM25.write(ppm25 + '\n')
-    f_average_PM25.close()
 
 ##PM10 Average##
 total_PM10 = 0.0
 length_PM10 = 0.0
-infile = open('/mnt/ramdisk_ram0/ppm10_average_sensor0', 'r')
-contents_PM10 = infile.read().strip().split()
-for num in contents_PM10:
-    total_PM10 += float(num)
-    length_PM10 += 1
-average_PM10 = total_PM10 / len(contents_PM10)
+
+for num in pm10_values:
+    amount = float(num)
+    total_PM10 += amount
+
+average_PM10 = total_PM10 / len(pm10_values)
 average_PM10 = float(format(average_PM10, ',.2f'))
 print("Average PM10 from 30 last measurements: ", average_PM10)
-infile.close()
+
 
 ##PM25 Average##
 total_PM25 = 0.0
-length_PM25 = 0.0
-infile = open('/mnt/ramdisk_ram0/ppm25_average_sensor0', 'r')
-contents_PM25 = infile.read().strip().split()
-for num in contents_PM25:
-    total_PM25 += float(num)
-    length_PM25 += 1
-average_PM25 = total_PM25 / len(contents_PM25)
+
+for num in pm25_values:
+    amount = float(num)
+    total_PM25 += amount
+
+average_PM25 = total_PM25 / len(pm25_values)
 average_PM25 = float(format(average_PM25, ',.2f'))
 print("Average PM25 from 30 last measurements: ", average_PM25)
-infile.close()
 
 json_body_public = [
     {
