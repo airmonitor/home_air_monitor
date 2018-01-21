2dbfba5e7aef712744a19516a1dd8eb98d548025
airmonitor <kontakt@skyphoto.pro>
2018-01-21 18:33:57 +0100

    update

--- lat_long.sh
+++ lat_long.sh
@@ -4,5 +4,5 @@ lat=`awk 'NR==1{print $1}' /boot/airmonitor.txt`
 long=`awk 'NR==2{print $1}' /boot/airmonitor.txt`
 echo $lat
 echo $long
-sed  -i "/lat/s/000000/${lat}/g" /etc/configuration/configuration.data
-sed  -i "/long/s/000000/${long}/g" /etc/configuration/configuration.data
+sed  -i "/lat/s/55.0000/${lat}/g" /etc/configuration/configuration.data
+sed  -i "/long/s/15.0000/${long}/g" /etc/configuration/configuration.data
