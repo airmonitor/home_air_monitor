2ead6d1457731960870b8a6da6db9f4cdb79ce74
Tomasz Szuster <>
2019-06-29 12:55:18 +0200

    Typo fixes.

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 set -e
-
-USER=`whoami`
-ssh-keygen
-ssh-copy-id ${USER}@localhost
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+ssh-copy-id `whoami`@localhost
 
 sudo apt update
 sudo apt install -y ansible git
@@ -13,6 +11,6 @@ sleep 5
 
 git clone https://github.com/airmonitor/home_air_monitor.git /home/pi/home_air_monitor
 
-/usr/bin/ansible-playbook /home/pi/home_air_monitor/ansible/main.yml
+/usr/bin/ansible-playbook /home/pi/home_air_monitor/ansible/roles/main.yml
 
 poweroff
