b1cbc107525ee739d0365a2bac9da604600709ed
Tomasz Szuster <tomasz.szuster@thomsonreuters.com>
2023-08-27 19:56:43 +0200

    :sparkles: Adding PTQS1005 support

--- micropython/main.py
+++ micropython/main.py
@@ -1,48 +1,44 @@
 # noinspection PyInterpreter
-from random import random
-
+import random
 import connect_wifi
-import contextlib
 import ujson
 import urequests
 import utime
-from boot import (
-    API_KEY,
-    API_URL,
-    LAT,
-    LONG,
-    PARTICLE_SENSOR,
-    SSID,
-    TEMP_HUM_PRESS_SENSOR,
-    TVOC_CO2_SENSOR,
-    WIFI_PASSWORD,
-)
+from boot import API_KEY, API_URL, LAT, LONG, PARTICLE_SENSOR, SSID, TEMP_HUM_PRESS_SENSOR, TVOC_CO2_SENSOR, WIFI_PASSWORD
 from i2c import I2CAdapter
 from lib import logging
 from machine import Pin, reset
 
-if TEMP_HUM_PRESS_SENSOR == "BME680":
-    import bme680
+logging.basicConfig()
+LOG = logging.getLogger(__name__)
 
+if TEMP_HUM_PRESS_SENSOR == "BME680":
+    LOG.info("Using BME680")
+    import bme680
 elif TEMP_HUM_PRESS_SENSOR == "BME280":
+    LOG.info("Using BME280")
     import bme280
 
-if PARTICLE_SENSOR in ["SDS011", "SDS021"]:
+if PARTICLE_SENSOR in ("SDS011", "SDS021"):
+    LOG.info("Using SDS0*")
     import sds011
-
     SDS = sds011.SDS011(uart=2)
 elif PARTICLE_SENSOR == "PMS7003":
+    LOG.info("Using PMS7003")
     from pms7003 import PassivePms7003
     from pms7003 import UartError
 
 if TVOC_CO2_SENSOR == "CCS811":
+    LOG.info("Using CCS811")
     import CCS811
 
+if PARTICLE_SENSOR == "PTQS1005":
+    LOG.info("Using PTQS1005")
+    from ptqs1005 import PTQS1005Sensor
 
-logging.basic_config(level=logging.INFO)
-LOG = logging.get_logger(__name__)
 
 LOOP_COUNTER = 0
+
 
 def sds_measurements():
     try:
@@ -66,13 +62,48 @@ def pms7003_measurements():
     except (OSError, UartError, TypeError):
         return {}
     finally:
-        with contextlib.suppress(OSError, UartError, TypeError, NameError):
+        try:
             pms.sleep()
+        except (OSError, UartError, TypeError, NameError):
+            pass
+
+
+def ptqs1005_measurements() -> dict:
+    try:
+        ptqs1005_sensor = PTQS1005Sensor(uart=2)
+        return ptqs1005_sensor.measure()
+    except (OSError, UartError, TypeError):
+        return {}
+
+
+def blink():
+    led = Pin(2, Pin.OUT)
+    led.value(1)
+    utime.sleep(0.1)
+    led.value(0)
+
+
+def blink_api_response(message):
+    if message.get("status") == "Metric saved":
+        LOG.info("Metric saved, blinking 2 times")
+        blink()
+        utime.sleep(0.1)
+        blink()
+    else:
+        LOG.info("Invalid request body, blinking 5 times")
+        blink()
+        utime.sleep(0.1)
+        blink()
+        utime.sleep(0.1)
+        blink()
+        utime.sleep(0.1)
+        blink()
+        utime.sleep(0.1)
+        blink()
 
 
 def send_measurements(data):
     LOG.info("Sending data to API %s", data)
-
     try:
         if data:
             post_data = ujson.dumps(data)
@@ -81,13 +112,12 @@ def send_measurements(data):
                 headers={"X-Api-Key": API_KEY, "Content-Type": "application/json"},
                 data=post_data,
             ).json()
-
             LOG.info("API response %s", res)
-
             blink_api_response(message=res)
             return True
     except IndexError:
         return False
+
 
 def get_particle_measurements():
     data = {}
@@ -99,17 +129,25 @@ def get_particle_measurements():
                 "pm25": round(particle_data["PM2_5_ATM"]),
                 "pm10": round(particle_data["PM10_0_ATM"]),
             }
-
-    elif PARTICLE_SENSOR in ["SDS011", "SDS021"]:
+    if PARTICLE_SENSOR == "PTQS1005":
+        particle_data = ptqs1005_measurements()
+        if particle_data:
+            data = {
+                "pm1": round(particle_data["pm10_atm"]),
+                "pm25": round(particle_data["pm25_atm"]),
+                "pm10": round(particle_data["pm100_atm"]),
+            }
+    if PARTICLE_SENSOR in ("SDS011", "SDS021"):
         particle_data = sds_measurements()
-
-        with contextlib.suppress(TypeError):
+        try:
             data = {
                 "pm25": round(particle_data["pm25"]),
                 "pm10": round(particle_data["pm10"]),
             }
-
+        except TypeError:
+            pass
     return data
+
 
 def get_tvoc_co2():
     if TVOC_CO2_SENSOR == "CCS811":
@@ -129,7 +167,6 @@ def get_temp_humid_pressure_measurements():
             sensor.set_pressure_oversample(bme680.OS_4X)
             sensor.set_temperature_oversample(bme680.OS_8X)
             sensor.set_filter(bme680.FILTER_SIZE_3)
-
             if sensor.get_sensor_data():
                 return {
                     "temperature": sensor.data.temperature,
@@ -139,13 +176,11 @@ def get_temp_humid_pressure_measurements():
                 }
         except (OSError, RuntimeError):
             return False
-
     elif TEMP_HUM_PRESS_SENSOR == "BME280":
         try:
             bme = bme280.BME280(i2c=i2c_dev)
             if bme.values:
                 LOG.info("BME280 readings %s", bme.values)
-
                 return {
                     "temperature": bme.values["temperature"],
                     "humidity": bme.values["humidity"],
@@ -161,66 +196,48 @@ def augment_data(measurements, sensor_name):
         data["lat"] = LAT
         data["long"] = LONG
         data["sensor"] = sensor_name
-
         return data
-
-
-def blink():
-    led = Pin(2, Pin.OUT)
-    led.value(1)
-    utime.sleep(0.1)
-    led.value(0)
-
-
-def blink_api_response(message):
-    if message.get("status") == "Metric saved":
-        blink()
-        utime.sleep(0.1)
-        blink()
-
 
 if __name__ == "__main__":
     if TEMP_HUM_PRESS_SENSOR:
         i2c_dev = I2CAdapter(scl=Pin(022), sda=Pin(021), freq=100000)
-
     while True:
         try:
+            LOG.info("Connecting to wifi...")
             connect_wifi.connect(ssid=SSID, password=WIFI_PASSWORD)
             utime.sleep(10)
+            LOG.info("Wifi connected")
             if PARTICLE_SENSOR:
-                # PARTICLE_SENSOR
-                parsed_values = augment_data(
+                LOG.info("Using particle sensor %s", PARTICLE_SENSOR)
+                values = augment_data(
                     measurements=get_particle_measurements(),
                     sensor_name=PARTICLE_SENSOR,
                 )
-                send_measurements(data=parsed_values)
-
+                send_measurements(data=values)
+                LOG.info("Parsed values %s", values)
                 utime.sleep(1)
-
-            # TEMP_HUM_PRESS SENSOR
             if TEMP_HUM_PRESS_SENSOR:
-                parsed_values = augment_data(
+                LOG.info("Using temp/humid/pressure sensor %s", TEMP_HUM_PRESS_SENSOR)
+                values = augment_data(
                     measurements=get_temp_humid_pressure_measurements(),
                     sensor_name=TEMP_HUM_PRESS_SENSOR,
                 )
-                send_measurements(data=parsed_values)
-
+                send_measurements(data=values)
                 utime.sleep(1)
-
-            # CO2 TVOC SENSOR
             if TVOC_CO2_SENSOR:
-                parsed_values = augment_data(
+                LOG.info("Using tvoc/co2 sensor %s", TVOC_CO2_SENSOR)
+                values = augment_data(
                     measurements=get_tvoc_co2(), sensor_name=TVOC_CO2_SENSOR
                 )
-
-                send_measurements(data=parsed_values)
-
+                send_measurements(data=values)
             LOOP_COUNTER += 1
             LOG.info("Increasing loop_counter, actual value %s", LOOP_COUNTER)
             if LOOP_COUNTER == 47:
+                LOG.info("Resetting device, loop counter %s", LOOP_COUNTER)
                 reset()
-
-            utime.sleep(int(random() * 600 + 1500))
+            random_sleep_value = random.randint(50, 59)
+            LOG.info("Sleeping for %s", random_sleep_value)
+            utime.sleep(random_sleep_value)
         except Exception as error:
             LOG.info("Caught exception %s", error)
             reset()
