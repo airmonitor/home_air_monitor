6cc338738706f9349004bdfbfbfbb8f1ab941ba7
Tomasz Szuster <tomasz.szuster@thomsonreuters.com>
2023-09-10 23:31:53 +0200

    :wrench: extending a file list that will be updated via OTA

--- micropython/home_air_monitor_ota.py
+++ micropython/home_air_monitor_ota.py
@@ -4,7 +4,19 @@ import micropython_ota
 def ota_updater():
     ota_host = 'https://static.airmonitor.pl'
     project_name = 'home_air_monitor_micropython'
-    filenames = ['ptqs1005.py']
+    filenames = [
+        "bme280.py",
+        "bme680.py",
+        "bme680_constants.py",
+        "ccs811.py",
+        "connect_wifi.py",
+        "home_air_monitor_ota.py",
+        "i2c.py",
+        "micropython_ota.py",
+        "pms7003.py",
+        "ptqs1005.py",
+        "sds011.py",
+    ]
 
     micropython_ota.ota_update(
         ota_host,
