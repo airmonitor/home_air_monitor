6e8566442cf074dda9d6e6e63e1e1815365f41d8
Tomasz Szuster <tomasz.szuster@thomsonreuters.com>
2023-09-25 10:46:58 +0200

    :recycle: Refactoring code.

--- micropython/ccs811.py
+++ micropython/ccs811.py
@@ -1,11 +1,11 @@
 """
-ams CCS811 Indoor Air Quality Sensor
+Ams CCS811 Indoor Air Quality Sensor
 Date: April 2018
 License: This code is public domain
 Based on CCS811 datasheet. Inspired by Adafruit and Sparkfun libraries
 Tested with Adafruit CCS811 Air quality breakout. Product ID: 3566
 Tested on NodeMCU and Wemos D1 Mini (ESP8266) MicroPython v1.8.7-7-gb5a1a20a3
-and MicroPython v1.9.3-8-g63826ac5c on 2017-11-01; ESP module with ESP8266
+and MicroPython v1.9.3-8-g63826ac5c in 2017-11-01; ESP module with ESP8266
 """
 
 
@@ -20,7 +20,7 @@ class CCS811(object):
         self.mode = 1  # Constant power mode; measurement every second
         self.error = False
 
-        # Check if sensor is vailable at i2c bus address
+        # Check if the sensor is available at i2c bus address
         devices = i2c.scan()
         if self.addr not in devices:
             raise ValueError(
@@ -43,61 +43,44 @@ class CCS811(object):
 
     def __string__(self):
         return "eCO2: %d ppm, TVOC: %d ppb" % (s.eCO2, s.tVOC)
-        # doesn't seem to work
 
     def data_ready(self):
-        """returns true if new data was downloaded. Values in .eCO2 and .tVOV"""
+        """Returns true if new data was downloaded. Values in .eCO2 and .tVOV"""
         status = self.i2c.readfrom_mem(self.addr, 0x00, 1)
         # bit 3 in the status register: data_ready
         if (status[0] >> 3) & 0x01:
             # datasheet Figure 14: Algorithm Register Byte Order (0x02)
             register = self.i2c.readfrom_mem(self.addr, 0x02, 4)
-            co2HB = register[0]
-            co2LB = register[1]
-            tVOCHB = register[2]
-            tVOCLB = register[3]
-            self.eCO2 = (co2HB << 8) | co2LB
-            self.tVOC = (tVOCHB << 8) | tVOCLB
+            co2_hb = register[0]
+            co2_lb = register[1]
+            tvoc_hb = register[2]
+            tvoc_lb = register[3]
+            self.eCO2 = (co2_hb << 8) | co2_lb
+            self.tVOC = (tvoc_hb << 8) | tvoc_lb
             return True
         else:
             return False
 
     def get_baseline(self):
         register = self.i2c.readfrom_mem(self.addr, 0x11, 2)
-        HB = register[0]
-        LB = register[1]
-        # baseline = (HB << 8) | LB
-        return HB, LB
+        hb = register[0]
+        lb = register[1]
+        return hb, lb
 
-    def put_baseline(self, HB, LB):
+    def put_baseline(self, hb, lb):
         register = bytearray([0x00, 0x00])
-        register[0] = HB
-        register[1] = LB
+        register[0] = hb
+        register[1] = lb
         self.i2c.writeto_mem(self.addr, 0x11, register)
 
-    def put_envdata(self, humidity, temp):
-        envregister = bytearray([0x00, 0x00, 0x00, 0x00])
-        envregister[0] = int(humidity) << 1
+    def put_env_data(self, humidity, temp):
+        env_register = bytearray([0x00, 0x00, 0x00, 0x00])
+        env_register[0] = int(humidity) << 1
         t = int(temp // 1)
         tf = temp % 1
-        t_H = (t + 25) << 9
-        t_L = int(tf * 512)
-        t_comb = t_H | t_L
-        envregister[2] = t_comb >> 8
-        envregister[3] = t_comb & 0xFF
-        self.i2c.writeto_mem(self.addr, 0x05, envregister)
-        # return envregister
-
-
-"""def main():
-    from machine import Pin, I2C
-    import time
-    i2c = I2C(scl=Pin(5), sda=Pin(4))
-    s = CCS811(i2c)
-    time.sleep(1)
-    while True:
-        if s.data_ready():
-            print('eCO2: %d ppm, TVOC: %d ppb' % (s.eCO2, s.tVOC))
-            time.sleep(1)
-
-main()"""
+        t_h = (t + 25) << 9
+        t_l = int(tf * 512)
+        t_comb = t_h | t_l
+        env_register[2] = t_comb >> 8
+        env_register[3] = t_comb & 0xFF
+        self.i2c.writeto_mem(self.addr, 0x05, env_register)
